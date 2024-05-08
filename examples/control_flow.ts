e08ebdc6593282042fa8af7b3f96070c6307d2f7
Harry Solovay <harrysolovay@gmail.com>
2024-05-08 11:02:43 -0400

    control flow example

---
+++ examples/control_flow.ts
@@ -0,0 +1,29 @@
+import * as L from "liminal"
+
+declare const maybe: L.u8 | L.None
+
+function match() {
+  const value = maybe.when(L.None, function*() {
+    return L.u8.from(0)
+  })
+  value satisfies L.u8
+}
+
+function* unhandle() {
+  const value = yield* maybe.unhandle(L.None)
+  value satisfies L.u8
+  return value
+}
+
+function handle() {
+  const effect = maybe.unhandle(L.None)
+  effect satisfies L.Effect<L.None, L.u8>
+
+  const handled = effect.handle(L.None, function*() {
+    return L.u8.from(0)
+  })
+  handled satisfies L.u8
+
+  const rehandled = effect.rehandle(L.None)
+  rehandled satisfies L.u8 | L.None
+}
