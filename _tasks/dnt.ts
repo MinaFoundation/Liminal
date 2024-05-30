5e06e030cc62f6fcf96261f6a8168a3f7a1381d6
Harry Solovay <harrysolovay@gmail.com>
2024-05-30 09:42:17 -0400

    configure NPM publish

---
+++ _tasks/dnt.ts
@@ -0,0 +1,41 @@
+import { build, emptyDir } from "@deno/dnt"
+import { parseArgs } from "@std/cli"
+
+const { version } = parseArgs(Deno.args, {
+  string: ["version"],
+  default: { version: "v0.0.0-local" },
+})
+
+await emptyDir("target/npm")
+await build({
+  package: {
+    name: "liminal",
+    version,
+    description: "",
+    license: "Apache-2.0",
+    repository: "github:MinaFoundation/Liminal",
+  },
+  compilerOptions: {
+    importHelpers: true,
+    sourceMap: true,
+    target: "ES2022",
+    lib: ["ES2022", "DOM"],
+  },
+  entryPoints: [
+    { name: ".", path: "./mod.ts" },
+    { name: "liminal/lib", path: "./lib/mod.ts" },
+    { name: "liminal/test", path: "./test/mod.ts" },
+  ],
+  outDir: "target/npm",
+  shims: {
+    deno: true,
+  },
+  test: false,
+  scriptModule: false,
+  async postBuild() {
+    await Promise.all([
+      Deno.copyFile("LICENSE", "target/npm/LICENSE"),
+      Deno.copyFile("README.md", "target/npm/README.md"),
+    ])
+  },
+})
