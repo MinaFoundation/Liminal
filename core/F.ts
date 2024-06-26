b23226229e8cb82076a0cf43fb28e5f25141b01c
Harry Solovay <harrysolovay@gmail.com>
2024-06-26 09:56:43 -0400

    continued

--- core/F.ts
+++ core/F.ts
@@ -1,25 +1,24 @@
 import { unimplemented } from "../util/unimplemented.ts"
-import { Authority } from "./Authority.ts"
 import { Result } from "./Call.ts"
 import { Effect } from "./Effect.ts"
-import { SignerRequirement } from "./Id.ts"
+import { PureProxy, SignerRequirement } from "./Id.ts"
 import { Value } from "./Value.ts"
 import { Vk, VkSource } from "./Vk.ts"
 
-export function effect<P extends Value.PropTypes, T, Y extends Value, R extends Result>(
+export function f<P extends Value.PropTypes, T, Y extends Value, R extends Result>(
   propTypes: P,
   statements: EffectStatements<T, [props: Value.PropsResolved<P>], Y, R>,
-): EffectType<P, T, Y, R>
-export function effect<T, Y extends Value, R extends Result>(
+): FType<P, T, Y, R>
+export function f<T, Y extends Value, R extends Result>(
   statements: EffectStatements<T, [props: {}], Y, R>,
-): EffectType<{}, T, Y, R>
-export function effect<P extends Value.PropTypes, T, Y extends Value, R extends Result>(
+): FType<{}, T, Y, R>
+export function f<P extends Value.PropTypes, T, Y extends Value, R extends Result>(
   propTypesOrStatements: P | EffectStatements<T, [props: {}], Y, R>,
   maybeStatements?: EffectStatements<T, [props: Value.PropsResolved<P>], Y, R>,
-): EffectType<P, T, Y, R> {
+): FType<P, T, Y, R> {
   return (typeof propTypesOrStatements === "function"
-    ? effectInternal({}, propTypesOrStatements)
-    : effectInternal(
+    ? fInternal({}, propTypesOrStatements)
+    : fInternal(
       propTypesOrStatements as never,
       maybeStatements as never,
     )) as never
@@ -32,14 +31,21 @@ export type EffectStatements<
   R extends Result,
 > = Generator<Y, R> | ((this: T, ...args: A) => Generator<Y, R>)
 
-export interface EffectType<
+export interface FType<
   P extends Value.PropTypes = any,
   T = any,
   Y extends Value = any,
   R extends Result = any,
-> extends ReturnType<typeof effectInternal<P, T, Y, R>> {}
+> extends ReturnType<typeof fInternal<P, T, Y, R>> {}
 
-export function effectInternal<
+export interface f<
+  P extends Value.PropTypes = any,
+  T = any,
+  Y extends Value = any,
+  R extends Result = any,
+> extends InstanceType<FType<P, T, Y, R>> {}
+
+export function fInternal<
   P extends Value.PropTypes,
   T,
   Y extends Value,
@@ -48,7 +54,7 @@ export function effectInternal<
   propTypes: P,
   statements: EffectStatements<T, [props: Value.PropsResolved<P>], Y, R>,
 ) {
-  return class extends Value.make("Effect")<never, string, EffectFrom<P>> {
+  return class extends Value.make("f")<never, string, FFrom<P>> {
     propTypes = propTypes
     statements = statements
     vk = new Vk(new VkSource(this))
@@ -59,14 +65,18 @@ export function effectInternal<
 
     authorize<K extends Extract<Y, SignerRequirement>["key"]>(
       _key: K,
-      _authority: Value.Args<[Authority]>[0],
-    ): InstanceType<EffectType<P, T, Exclude<Y, SignerRequirement<K>>, R>> {
+      _authority: Value.Args<[PureProxy]>[0],
+    ): f<P, T, Exclude<Y, SignerRequirement<K>>, R> {
+      unimplemented()
+    }
+
+    sign<K extends string>(_key: K): f<P, T, Y | SignerRequirement<K>, R> {
       unimplemented()
     }
   }
 }
 
-export type EffectFrom<P extends Value.PropTypes = any> = {
+export type FFrom<P extends Value.PropTypes = any> = {
   [K in keyof P]:
     | InstanceType<P[K]>
     | Value.From<InstanceType<P[K]>>
